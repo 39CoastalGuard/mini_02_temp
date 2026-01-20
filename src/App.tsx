3fa6a85451bd9d0d373ccf4405305bed9c238a7f
LKB <ggubo01@gmail.com>
2026-01-20 14:50:26 +0900

    삭제 전 알림기능

--- src/App.tsx
+++ src/App.tsx
@@ -60,11 +60,19 @@ export default function App() {
   };
 
   const handleDeletePost = () => {
-    if (!selectedPost) return;
-    setPosts(posts.filter((p) => p.id !== selectedPost.id));
-    setSelectedPost(null);
-    window.alert(`"${selectedPost.title}"가 삭제되었습니다 🗑️`);
-  };
+  if (!selectedPost) return;
+
+  const ok = window.confirm(
+    `"${selectedPost.title}"를 정말 삭제하시겠습니까?`
+  );
+
+  if (!ok) return; // 취소 누르면 종료
+
+  setPosts(posts.filter((p) => p.id !== selectedPost.id));
+  setSelectedPost(null);
+  window.alert(`"${selectedPost.title}"가 삭제되었습니다 🗑️`);
+};
+
 
   const handleBuy = () => {
     if (selectedPost) {
